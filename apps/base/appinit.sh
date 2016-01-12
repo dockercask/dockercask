e911b0dc0768f902217bff0a30affe9b1bc25bd3
Zachary Huff <zach.huff.386@gmail.com>
2016-01-12 16:57:01 -0500

    Overide base settings with app settings

--- apps/base/appinit.sh
+++ apps/base/appinit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-sudo -HEu docker sed -i "s/{{ gtk2_theme }}/`jq -r .gtk2_theme base.json`/" /home/docker/.gtkrc-2.0
-sudo -HEu docker sed -i "s/{{ gtk2_font }}/`jq -r .gtk2_font base.json`/" /home/docker/.gtkrc-2.0
-sudo -HEu docker sed -i "s/{{ gtk2_icon_theme }}/`jq -r .gtk2_icon_theme base.json`/" /home/docker/.gtkrc-2.0
-sudo -HEu docker sed -i "s/{{ gtk2_cursor_theme }}/`jq -r .gtk2_cursor_theme base.json`/" /home/docker/.gtkrc-2.0
+sudo -HEu docker sed -i "s/{{ gtk2_theme }}/`getsetting gtk2_theme`/" /home/docker/.gtkrc-2.0
+sudo -HEu docker sed -i "s/{{ gtk2_font }}/`getsetting gtk2_font`/" /home/docker/.gtkrc-2.0
+sudo -HEu docker sed -i "s/{{ gtk2_icon_theme }}/`getsetting gtk2_icon_theme`/" /home/docker/.gtkrc-2.0
+sudo -HEu docker sed -i "s/{{ gtk2_cursor_theme }}/`getsetting gtk2_cursor_theme`/" /home/docker/.gtkrc-2.0
 
-sudo -HEu docker sed -i "s/{{ gtk3_theme }}/`jq -r .gtk3_theme base.json`/" /home/docker/.config/gtk-3.0/settings.ini
-sudo -HEu docker sed -i "s/{{ gtk3_font }}/`jq -r .gtk3_font base.json`/" /home/docker/.config/gtk-3.0/settings.ini
-sudo -HEu docker sed -i "s/{{ gtk3_icon_theme }}/`jq -r .gtk3_icon_theme base.json`/" /home/docker/.config/gtk-3.0/settings.ini
-sudo -HEu docker sed -i "s/{{ gtk3_cursor_theme }}/`jq -r .gtk3_cursor_theme base.json`/" /home/docker/.config/gtk-3.0/settings.ini
+sudo -HEu docker sed -i "s/{{ gtk3_theme }}/`getsetting gtk3_theme`/" /home/docker/.config/gtk-3.0/settings.ini
+sudo -HEu docker sed -i "s/{{ gtk3_font }}/`getsetting gtk3_font`/" /home/docker/.config/gtk-3.0/settings.ini
+sudo -HEu docker sed -i "s/{{ gtk3_icon_theme }}/`getsetting gtk3_icon_theme`/" /home/docker/.config/gtk-3.0/settings.ini
+sudo -HEu docker sed -i "s/{{ gtk3_cursor_theme }}/`getsetting gtk3_cursor_theme`/" /home/docker/.config/gtk-3.0/settings.ini
 
-sudo -HEu docker sed -i "s/{{ xfwm4_theme }}/`jq -r .xfwm4_theme base.json`/" /home/docker/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
-sudo -HEu docker sed -i "s/{{ xfwm4_font }}/`jq -r .xfwm4_font base.json`/" /home/docker/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+sudo -HEu docker sed -i "s/{{ xfwm4_theme }}/`getsetting xfwm4_theme`/" /home/docker/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+sudo -HEu docker sed -i "s/{{ xfwm4_font }}/`getsetting xfwm4_font`/" /home/docker/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
 
 sudo -HEu docker touch /tmp/.Xauth
 sudo -HEu docker xauth -f /tmp/.Xauth add $DISPLAY MIT-MAGIC-COOKIE-1 $XCOOKIE

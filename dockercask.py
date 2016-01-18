9bd21aac0134e15b76e1181aa3f86f2f53cde259
Zachary Huff <zach.huff.386@gmail.com>
2016-01-17 20:23:14 -0500

    Change clipboard

--- dockercask.py
+++ dockercask.py
@@ -417,41 +417,17 @@ def run(app):
             clean_up()
 
 def set_clipboard(num, val):
-    process = subprocess.Popen(
+    proc = subprocess.Popen(
         ['xsel', '--display', ':' + num, '-b', '-i', '-t', '450'],
         stdin=subprocess.PIPE,
     )
-    process.stdin.write(val)
-    process.stdin.close()
-
-    for _ in xrange(100):
-        time.sleep(0.005)
-        exit_code = process.poll()
-        if exit_code is not None:
-            if exit_code != 0:
-                raise Exception('Error from xsel process')
-            return
-
-    process.kill()
-    raise Exception('Timeout setting clipboard')
+    proc.communicate(input=val)
+    proc.wait()
 
 def get_clipboard(num):
-    process = subprocess.Popen(
+    return subprocess.check_output(
         ['xsel', '--display', ':' + num, '-b', '-o', '-t', '450'],
-        stdout=subprocess.PIPE,
     )
-
-    for _ in xrange(100):
-        time.sleep(0.005)
-        exit_code = process.poll()
-        if exit_code is not None:
-            if exit_code != 0:
-                raise Exception('Error from xsel process')
-            output, _ = process.communicate()
-            return output
-
-    process.kill()
-    raise Exception('Timeout getting clipboard')
 
 def share_clipboard(app_num):
     time.sleep(1)

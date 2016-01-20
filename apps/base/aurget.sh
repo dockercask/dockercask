8e0a6bdd2f0b233378cc0a18ea35054f2f1e045b
Zachary Huff <zach.huff.386@gmail.com>
2016-01-19 22:41:00 -0500

    Add support for gpg keys in aurget

--- apps/base/aurget.sh
+++ apps/base/aurget.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-useradd pacman
+set -e
+useradd -m pacman
 echo "pacman ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 cd /tmp
+if [ -n "$2" ]
+then
+    sudo -u pacman gpg --keyserver $2 --recv-keys $3
+fi
 sudo -u pacman wget https://aur4.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
 sudo -u pacman tar xfz $1.tar.gz
 rm $1.tar.gz
@@ -11,5 +16,5 @@ mkdir -p /aur
 cp $1*.pkg.tar.xz /aur
 cd ..
 rm -rf $1
-userdel pacman
+userdel -r pacman
 sed -i '/pacman ALL=(ALL) NOPASSWD: ALL/d' /etc/sudoers

0ac2a356c8ef871955cd8301d91f5ab3b6c8ec42
Zachary Huff <zach.huff.386@gmail.com>
2016-04-15 05:14:44 -0400

    Update aurget

--- apps/base/aurget.sh
+++ apps/base/aurget.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
-useradd -m pacman
+useradd -m pacman | true
+sed -i '/pacman ALL=(ALL) NOPASSWD: ALL/d' /etc/sudoers
 echo "pacman ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 cd /tmp
 if [ -n "$2" ]
 then
     sudo -u pacman gpg --keyserver $2 --recv-keys $3
 fi
-sudo -u pacman wget https://aur4.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
+sudo -u pacman wget https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
 sudo -u pacman tar xfz $1.tar.gz
 rm $1.tar.gz
 cd $1

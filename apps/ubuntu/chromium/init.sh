ee5c2fb25cca9a67b41c26356253e6c4446423ff
Zachary Huff <zach.huff.386@gmail.com>
2016-08-18 04:22:05 -0400

    Fix ubuntu chromium init

--- apps/ubuntu/chromium/init.sh
+++ apps/ubuntu/chromium/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 appinitpanel
 ln -s -T /home/docker/Docker /home/docker/.config/chromium
-chromium --no-sandbox --disable-gpu --disable-flash-3d
+chromium-browser --no-sandbox --disable-gpu --disable-flash-3d

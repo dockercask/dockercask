29fd3c731a27ecdb44fc7becf7dd5e3c64083217
Zachary Huff <zach.huff.386@gmail.com>
2017-02-17 02:12:20 -0500

    Add mount_path to ubuntu apps

--- apps/ubuntu/chrome/init.sh
+++ apps/ubuntu/chrome/init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 appinitpanel
-ln -s -T /home/docker/Docker /home/docker/.config/google-chrome
 google-chrome-stable --no-sandbox --disable-gpu --disable-flash-3d
--- apps/ubuntu/chromium/init.sh
+++ apps/ubuntu/chromium/init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 appinitpanel
-ln -s -T /home/docker/Docker /home/docker/.config/chromium
 chromium-browser --no-sandbox --disable-gpu --disable-flash-3d
